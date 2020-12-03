ba36b10802cba685dc2dbe2366291fd26de16dd7
Kevin Costa <kevincosta@alu.ufc.br>
2020-12-03 18:12:00 -0300

    Solving Erros

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :log_level, :debug
 append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "storage", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
