1f9cc1abb138646f356fc9a7853603e678d2cd53
Kevin Costa <kevincosta@alu.ufc.br>
2020-12-07 15:11:06 -0300

    ACtionCable Fix2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,9 +53,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  config.action_cable.url = 'ws://dgmeter.online/cable'
+  config.action_cable.allowed_request_origins = [ 'http://dgmeter.online', /http:\/\/dgmeter.*/ ]
+  # config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
