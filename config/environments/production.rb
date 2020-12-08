dd6213e98a8194bc536df84e346bbfbdeb513106
Kevin Costa <kevincosta@alu.ufc.br>
2020-12-08 15:22:04 -0300

    ActionCable new tries

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://dgmeter.online/cable'
+  config.action_cable.url = 'wss://dgmeter.online/cable'
   config.action_cable.allowed_request_origins = [ 'http://dgmeter.online', /http:\/\/dgmeter.*/ ]
   # config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
